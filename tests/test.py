944e7775464977cd03d350b969627c3fb4490757
Tony Smith <smittytone@users.noreply.github.com>
2022-08-16 06:02:10 +0100

    Add tests

---
+++ tests/test.py
@@ -0,0 +1,12 @@
+import unittest
+
+from openweather import OpenWeather
+
+class TestSetUnit():
+
+    def test_correct(self):
+        
+        
+        
+if __name__ == "__main__":
+    unittest.main()
